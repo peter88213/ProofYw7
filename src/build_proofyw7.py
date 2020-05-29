ea8103de29c57d5cd51de94118e942cb1d03f0e0
peter88213 <petert88213@gmail.com>
2020-05-29 23:53:39 +0200

    Refactor

--- src/build_proofyw7.py
+++ src/build_proofyw7.py
@@ -3,7 +3,10 @@
 In order to distribute single scripts without dependencies, 
 this script "inlines" all modules imported from the pywriter package.
 
-For further information see https://github.com/peter88213/PyWriter
+The PyWriter project (see see https://github.com/peter88213/PyWriter)
+must be located on the same directory level as the ProofYw7 project. 
+
+For further information see https://github.com/peter88213/ProofYw7
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
